3fc3f7191703244401df9a354f5ec9b8da04caef
Satoshi Tanaka <tanarky@gmail.com>
2017-05-29 01:55:56 +0900

    added admin pages

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,19 +1,59 @@
 # coding: utf-8
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for, current_app, request
 import logging, sys, os, traceback
-from models import BlogArticle
+from datetime import datetime, tzinfo
+from models import Page, PagePhoto
 
 app_admin = Blueprint('app_admin', __name__, url_prefix='/admin')
 
 @app_admin.route('/', methods=['GET'])
 def top():
-    BlogArticle.query().fetch()
-    #b = BlogArticle(
-    #    id = "1",
-    #    title = "テストタイトル",
-    #    body  = "本文 あああ いいい",
-    #    tags = ["小田急線", "世田谷線"],
-    #    published_at = None
-    #)
-    #b.put()
     return render_template('admin_top.html')
+
+@app_admin.route('/pages', methods=['GET'])
+def pages():
+    pages = Page().query(Page.page_type == 'article').order(-Page.updated_at)
+    return render_template('admin_pages.html', T={'pages':pages})
+
+@app_admin.route('/pages/new', methods=['GET'])
+def pages_new():
+    return render_template('admin_pages_new.html', T={})
+
+@app_admin.route('/pages/<page_id>/edit', methods=['GET'])
+def pages_edit(page_id):
+    p = Page().get_by_id(int(page_id))
+    return render_template('admin_pages_edit.html', T={'page':p})
+
+@app_admin.route('/pages/<page_id>', methods=['POST'])
+def pages_update(page_id):
+    try:
+        p = Page().get_by_id(int(page_id))
+        p.title     = request.form['title']
+        p.body      = request.form['body']
+        p.page_type = request.form['page_type']
+        if request.form['published_at'] and request.form['published_at'] != '':
+            d = datetime.strptime(request.form['published_at'],
+                                  '%Y-%m-%d %H:%M:%S')
+            p.published_at = d
+        p.put()
+        flash('page updated', 'success')
+    except Exception as e:
+        current_app.logger.info(e)
+        flash('page save error', 'error')
+    return redirect(url_for('app_admin.pages_edit', page_id=page_id))
+
+@app_admin.route('/pages/create', methods=['POST'])
+def pages_create():
+    try:
+        p = Page(
+            title=request.form['title'],
+            body=request.form['body'],
+            page_type=request.form['page_type'],
+        )
+        p.put()
+        flash('page created', 'success')
+        return redirect(url_for('app_admin.pages_edit', page_id=p.key.id()))
+    except Exception as e:
+        current_app.logger.info(e)
+        flash('page save error', 'error')
+    return redirect(url_for('app_admin.pages'))
--- models.py
+++ models.py
@@ -1,9 +1,18 @@
 # coding: utf-8
 from google.appengine.ext import ndb
 
-class BlogArticle(ndb.Model):
+class PagePhoto(ndb.Model):
+    path = ndb.StringProperty()
+    published_at = ndb.DateProperty()
+    created_at   = ndb.DateTimeProperty(auto_now_add=True)
+
+class Page(ndb.Model):
     title        = ndb.StringProperty()
     body         = ndb.TextProperty()
     tags         = ndb.StringProperty(repeated=True)
-    published_at = ndb.DateProperty()
+    photos       = ndb.StructuredProperty(PagePhoto, repeated=True)
+    published_at = ndb.DateTimeProperty()
+    page_type    = ndb.StringProperty(choices=['static', 'article'], default="article")
+    updated_at   = ndb.DateTimeProperty(auto_now=True)
     created_at   = ndb.DateTimeProperty(auto_now_add=True)
+
