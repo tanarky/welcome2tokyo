da9b65ac83f06397d7d8099cc1e81726e58143c8
Satoshi Tanaka <tanarky@gmail.com>
2017-06-11 09:37:47 +0900

    fixed

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 import os
 from admin import app_admin
+from google.appengine.ext import ndb
 
 app = Flask(__name__)
 app.secret_key = os.environ['FLASK_SECRET_KEY']
@@ -11,6 +12,11 @@ app.register_blueprint(app_admin)
 @app.route('/')
 def doc_root():
     return render_template('pages_top.html')
+
+@app.route('/p/<page_key>.html')
+def show_page(page_key):
+    page = ndb.Key(urlsafe=page_key).get()
+    return render_template('pages_show.html', T={'page':page})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
